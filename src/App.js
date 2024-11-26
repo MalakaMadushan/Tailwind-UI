bd767700b2f9287332ad790942b238311f069c3a
Malaka <malakamadushan123@gmail.com>
2024-11-26 14:23:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import SignForm from './SignForm';
+
+function App() {
+  return (
+    <div className="App min-h-screen bg-gray-100 flex items-center justify-center">
+      <SignForm />
+    </div>
+  );
+}
+
+export default App;
+
